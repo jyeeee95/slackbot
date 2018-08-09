7e2d8705b39517fa80f03c61fe6243cc77b21ad2
jyeeee95 <jihye950415@gmail.com>
2018-08-09 11:32:49 +0900

    Change some syntax for Simplicity and Utilization of Code

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ from rtmbot.core import Plugin
 import secret
 
 
+def answer(text):
+    if "지혜" in text:
+        reply = "나 불렀어?"
+    elif "주사위" == text:
+        reply = str(random.randint(1,6))
+    else:
+        reply = None
+    return reply
+
+
 class HelloPlugin(Plugin):
-    def process_message(self, data):
-        if "애란" in data["text"]:
-            self.outputs.append([data["channel"], "불렀어?"])
-        elif "주사위" == data["text"]:
-            die = str(random.randint(1, 6))
-            self.outputs.append([data["channel"], die])
-        else:
-            pass
+    def process_message(self,data):
+        reply = answer(data["text"])
+        if reply is not None:
+            self.outputs.append([data["channel"],reply])
 
 
 config = {
     "SLACK_TOKEN": secret.SLACK_TOKEN,
     "ACTIVE_PLUGINS": ["main.HelloPlugin"]
 }
+
+
 bot = RtmBot(config)
 bot.start()
